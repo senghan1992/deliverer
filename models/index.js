89ec9673fafbd6e4ba18644ecfc8e811e9a91387
변승환 <senghan1992@byeonseunghwan-ui-MacBookPro.local>
2019-11-03 19:41:21 +0900

    modify envs file

--- models/index.js
+++ models/index.js
@@ -41,7 +41,7 @@ const Op = Sequelize.Op;
 //   $col: Op.col,
 // };
 // const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'test';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
