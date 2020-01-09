67f5643fc2c4fb0149f3fb9763e6881ddb8b9939
변승환 <senghan1992@gmail.com>
2020-01-09 15:54:00 +0900

    add cancel table

---
+++ models/cancel.js
@@ -0,0 +1,23 @@
+const moment = require("moment");
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("cancel", {
+    // 취소한 요청 건 id
+    orderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: "취소한 요청 건 id"
+    },
+    // 취소당시 요청의 상태값
+    order_status: {
+      type: DataTypes.ENUM("A", "B", "C", "D", "E", "F"),
+      allowNull: false,
+      comment: "취소 요청 당시 발송의 상태값"
+    },
+    // 취소자 id
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: "취소한 유저 id"
+    }
+  });
+};
